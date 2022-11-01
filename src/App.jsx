502f6ff4c7fce130981bd4af99744a442c201688
Katalin Czifferi <katica@Katalins-Air.lan>
2022-11-01 21:08:08 +0100

    useeffect logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   
-  const initialText = "Click me!"
-  const [counter, setCounter] = useState(1);
-  const [btnText, setBtnText] = useState(initialText);
-  function handleClick() {
-    
-    console.log(counter);
-    if (counter == 1) {
-      console.log(counter)
-    setBtnText("You clicked me!");
-    }
-    if (counter == 2) {
-      setBtnText("Stop clicking me")
-    }
+  const [counter, setCounter] = useState(0);
+  
+  useEffect(() => {
+    if (counter) 
+      var timer = setTimeout(() => setCounter(0), 3000);
+    return () => clearTimeout(timer);
+  }, [counter]);
 
-    setTimeout(() => {
-      setBtnText(initialText);
-    }, 3000);
-  } 
-
-
-  return (
+  
+return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to React
         </p>
-        <button onClick={() => {setCounter(counter+1); handleClick();}}>{btnText}</button>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+
+        <button onClick={() => {setCounter(counter+1)}} >
+          {counter === 0 && 'Click me!'}
+          {counter === 1 && 'You clicked me!'}
+          {counter >= 2 && 'Stop clicking me!'}
+        </button>
+
       </header>
     </div>
   );
 }
 
 export default App;
+
+// onClick={(e) => {setCounter(counter+1); handleClick(e);}}
